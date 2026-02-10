753469b17d7737b0eaf40aa84dfb52cf0e555cab
Sebastian <sebik2210@gmail.com>
2026-02-10 19:12:16 +0100

    Added theme support

--- Program.cs
+++ Program.cs
@@ -9,6 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<Camera>();
-builder.Services.AddSingleton<Editor>();
+builder.Services.AddScoped<Editor>();
 
 await builder.Build().RunAsync();
